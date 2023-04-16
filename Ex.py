ef192bdb2d64843e855a0acbd1a0a5f50121234c
Choi-Seong-Hyeok <cinemay3@daum.net>
2023-04-16 20:12:35 +0900

    [BOJ] 이분탐색 provlem solved ..

--- Ex.py
+++ Ex.py
@@ -1,25 +1,2 @@
-import sys
-input = sys.stdin.readline
-'''
-D[i] : 마지막으로 뽑은 수가 a[i]일때 가장 긴 부분수열의 길이
 
-지금까지 만들어 놓은 증가수열에 a[i]를 붙일 수 있으려면
-    - 만들어 놓은 증가수열이 a[i]보다 앞에 있어야하고 (j < i)
-    - 마지막 수가 a[i]보다 작아야 한다.(a[j] < a[i])
-'''
-n = int(input())
-arr = list(map(int, input().split()))
-
-# dp 테이블 1로 초기화
-dp = [1 for _ in range(n)]
-
-for i in range(1,n):
-    for j in range(0,i):
-        if arr[j] < arr[i]:
-            # 더 긴 길이를 찾아가는 과정 (?)
-            dp[i] = max(dp[i],dp[j]+1)
-
-result = max(dp)
-print(result)
-
-
+print(50//100)
--- 이분탐색/랜선자르기.py
+++ 이분탐색/랜선자르기.py
@@ -1,31 +1,33 @@
 import sys
+input = sys.stdin.readline
 
-# K개의 랜선과 필요로하는 랜선의 개수 N
-K,N = map(int,sys.stdin.readline().split())
+k,n = map(int,input().rstrip().split())
+LAN = []
+# 랜선의 길이
+for _ in range(k):
+    LAN.append(int(input()))
+# 오름차순 정렬
+LAN.sort()
+# start 값은 0이될수없다
+left = 1
+# end 값은 max(LAN)
+right = LAN[k-1]
 
-LANCable = []
-for i in range(K):
-    val = int(sys.stdin.readline())
-    LANCable.append(val)
-
-LANCable.sort()
-
-start = 0
-end = max(LANCable)
-result = 0
-while start <= end:
-
-    mid = (start + end)//2
+while left <= right:
+    # target value
+    mid = (left + right) // 2
     cnt = 0
-
-    for lan in LANCable:
-        if lan >= mid:
-            cnt += lan // mid
-    if cnt >= N:
-        result = mid
-        start = mid + 1
+    # 랜선의 길이를 mid로 잘랐을때 길이 구하기
+    for i in LAN:
+        if i >= mid:
+            cnt += (i // mid)
+    # 생각보다 덜 잘렸으면 right를 앞으로 땡겨
+    if cnt < n:
+        right = mid - 1
+    # 생각보다 더 잘렸으면 left를 뒤로 땡겨
     else:
-        end = mid - 1
+        left = mid + 1
 
 
-print(end)
+print(right)
+

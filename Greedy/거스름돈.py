d98863e90e32488b3112d70ce9e83cc6b99e536b
Choi-Seong-Hyeok <cinemay3@daum.net>
2023-08-19 01:56:43 +0900

    [BOJ] 폴리오미노.py

--- Greedy/거스름돈.py
+++ Greedy/거스름돈.py
@@ -6,7 +6,7 @@ cnt = 0
 val = n // 5
 remainder = 0
 while 1:
-    if n == 1 or n == 3:
+    if n == 1 or n == 3: # n이 1 or 3이면 pass
         cnt = -1
         break
     remainder = n - (5 * val)
@@ -14,12 +14,11 @@ while 1:
     if remainder == 0: # 나머지가 0
         break
     if remainder % 2 == 0: # 나머지가 2의 배수로 떨어지는경우 경우
-        val -= 1
         cnt += (remainder // 2)
         break
     else:
-        val -= 1
-        cnt = 0
+        val -= 1 # 몫을 1 줄임
+        cnt = 0 # cnt 값은 초기화
 
 
 print(cnt)

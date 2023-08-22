50fc98f4cd904d598c49642fb5cc6da7cabbad6f
Choi-Seong-Hyeok <cinemay3@daum.net>
2023-08-22 11:29:37 +0900

    [Programmers] 문자열 계산하기

--- Programmers/도서실습/문자열계산하기.py
+++ Programmers/도서실습/문자열계산하기.py
@@ -1,15 +1,10 @@
-
 def solution(my_string):
-    
+    my_string = my_string.split()
     answer = int(my_string[0])
     
-    for i in range(len(my_string)):
-        if my_string[i-1] == '+':
-            answer = answer + int(my_string[i])
-        if my_string[i-1] == '-':
-            answer = answer - int(my_string[i])
+    for i in range(len(my_string)):  
+        if my_string[i] == '+':
+            answer = answer + int(my_string[i+1])
+        if my_string[i] == '-':
+            answer = answer - int(my_string[i+1])
     return answer
-
-my_string = "3+4"
-
-print(solution(my_string))

7415d47a07d8febcc758af3801089c1bde3ab85b
Choi-Seong-Hyeok <cinemay3@daum.net>
2023-09-06 17:59:04 +0900

    [BOJ] 안전영역

--- DFS/안전영역(rt).py
+++ DFS/안전영역(rt).py
@@ -1,27 +1,22 @@
 '''
  기준 값을 몇으로 둘것이냐 ?
- --> 배열의 최소값과 최대값사이에 모든 값들
+ --> 배열의 최소값과 최대값사이에 모든 값들(X)
+ --> 0부터 최대값 사이에 모든 값들(O)
 '''
+
 import sys
 sys.setrecursionlimit(100000)
 
 rainList = [] # 기준 장마 리스트
 resultList = [] # 각 영역마다 리스트 
-maxs = 0
-mins = float('inf')
+maxs = 0 # 장마 리스트 안의 최대값
 N = int(input())
 
 for i in range(N):
     val = list(map(int,input().split()))
 
-    if maxs > max(val):
-        pass
-    elif max(val) > maxs:
+    if max(val) > maxs:
         maxs = max(val)
-    if mins < min(val):
-        pass
-    elif mins > min(val):
-        mins = min(val)
     rainList.append(val)
 
 
@@ -38,34 +33,31 @@ def dfs(x,y,mins,visited):
     return False
 
 visited = []
-min = 0
-for _ in range(N):
+for _ in range(N): # 방문 리스트 초기화
      val = [0] * N
      visited.append(val)
 
-i = 0
-while(i <= maxs):
+small = 1 # 1부터 max까지 모든 경우의 수를 구함
+while(small < maxs):
     cnt = 0
     for i in range(N): # 방문 리스트 작성
          for j in range(N):
-              if rainList[i][j] > mins:
+              if rainList[i][j] > small:
                    visited[i][j] = True
               else:
                    visited[i][j] = False
 
     for i in range(N):
         for j in range(N):
-            if rainList[i][j] > mins:
-                if dfs(i,j,mins,visited) == True:
+            if rainList[i][j] > small:
+                if dfs(i,j,small,visited) == True:
                         cnt += 1
 
     resultList.append(cnt)
-    i += 1
+    small += 1
 
-print(max(resultList))
-
-     
-
-        
-
+if resultList:
+    print(max(resultList))
+else:
+    print(1)
 
